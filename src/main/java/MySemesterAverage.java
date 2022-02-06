3043ef57971125e8935e3edf630b07f1f01ae6a4
Gyuris Csaba <gycsaba72@gmail.com>
2022-02-06 20:17:11 +0100

    Update MySemesterAverage.java

--- src/main/java/MySemesterAverage.java
+++ src/main/java/MySemesterAverage.java
@@ -4,11 +4,6 @@ public class MySemesterAverage {
 
 	public static void main(final String[] args) {
 		// Write your solution here
-		System.out.print("Adja meg az elso feleves atlagat:");
-		Scanner in=new Scanner(System.in);
-		double average=in.nextDouble();
-		System.out.println(average);
-		in.close();
 	}
 
 }
